61cf4f7b4910066eda00c3f4ff30c676a5756645
Christopher N. Hesse <raymanfx@gmail.com>
2020-05-01 19:01:05 +0200

    examples: Add CaptureDevice example
    Rename the previous example to just 'list', because that's what it does.
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

---
+++ examples/device.rs
@@ -0,0 +1,37 @@
+extern crate clap;
+extern crate v4l;
+
+use clap::{App, Arg};
+use v4l::CaptureDevice;
+
+fn main() {
+    let matches = App::new("v4l device")
+        .version("0.2")
+        .author("Christopher N. Hesse <raymanfx@gmail.com>")
+        .about("Video4Linux device example")
+        .arg(
+            Arg::with_name("device")
+                .short("d")
+                .long("device")
+                .value_name("INDEX or PATH")
+                .help("Device node path or index (default: 0)")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    // Determine which device to use
+    let mut path: String = matches
+        .value_of("device")
+        .unwrap_or("/dev/video0")
+        .to_string();
+    if path.parse::<u64>().is_ok() {
+        path = format!("/dev/video{}", path);
+    }
+    println!("Using device: {}\n", path);
+
+    let dev = CaptureDevice::with_path(path).expect("Failed to open device");
+    let format = dev.get_format().expect("Failed to get format");
+    let params = dev.get_params().expect("Failed to get parameters");
+    println!("Active format:\n{}", format);
+    println!("Active parameters:\n{}", params);
+}
