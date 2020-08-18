618c0a24e0478bb7116c9c8a1f707ae36baa70d2
nvarner <nathanmvarner@protonmail.com>
2020-08-18 10:03:21 -0400

    Add support for premultiplying alpha in Format

--- src/capture/format.rs
+++ src/capture/format.rs
@@ -24,6 +24,8 @@ pub struct Format {
     pub size: u32,
     /// colorspace of the pixels
     pub colorspace: Colorspace,
+    /// premultiply alpha; ie. if the alpha channel is 50%, multiply all other channels by 50%
+    pub premultiply_alpha: bool,
     /// the quantization for the colorspace
     pub quantization: Quantization,
     /// the transfer function for the colorspace
@@ -55,6 +57,7 @@ impl Format {
             stride: 0,
             size: 0,
             colorspace: Colorspace::Default,
+            premultiply_alpha: false,
             quantization: Quantization::Default,
             transfer_function: TransferFunction::Default,
         }
@@ -70,6 +73,7 @@ impl fmt::Display for Format {
         writeln!(f, "stride            : {}", self.stride)?;
         writeln!(f, "size              : {}", self.size)?;
         writeln!(f, "colorspace        : {}", self.colorspace)?;
+        writeln!(f, "premultiply alpha : {}", self.premultiply_alpha)?;
         writeln!(f, "quantization      : {}", self.quantization)?;
         writeln!(f, "transfer function : {}", self.transfer_function)?;
         Ok(())
@@ -87,6 +91,7 @@ impl From<v4l2_pix_format> for Format {
             stride: fmt.bytesperline,
             size: fmt.sizeimage,
             colorspace: Colorspace::try_from(fmt.colorspace).expect("Invalid colorspace"),
+            premultiply_alpha: (fmt.flags & 0x1) == 0x1,
             quantization: Quantization::try_from(fmt.quantization).expect("Invalid quantization"),
             transfer_function: TransferFunction::try_from(fmt.xfer_func)
                 .expect("Invalid transfer function"),
@@ -108,6 +113,7 @@ impl Into<v4l2_pix_format> for Format {
         fmt.bytesperline = self.stride;
         fmt.sizeimage = self.size;
         fmt.colorspace = self.colorspace as u32;
+        fmt.flags = if self.premultiply_alpha { 0x1 } else { 0x0 };
         fmt.quantization = self.quantization as u32;
         fmt.xfer_func = self.transfer_function as u32;
         fmt
