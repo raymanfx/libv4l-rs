d6e29b5127c2eeacf0b0c6b175d036ec22fa84f2
Christopher N. Hesse <raymanfx@gmail.com>
2020-08-26 12:40:11 +0200

    format: Add support for flags field in Format
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- src/format.rs
+++ src/format.rs
@@ -10,11 +10,72 @@ use crate::v4l_sys::*;
 
 bitflags! {
     #[allow(clippy::unreadable_literal)]
-    pub struct Flags : u32 {
+    pub struct DescriptionFlags : u32 {
         const COMPRESSED            = 0x0001;
         const EMULATED              = 0x0002;
         const CONTINUOUS_BITSTREAM  = 0x0004;
         const DYN_RESOLUTION        = 0x0008;
+    }
+}
+
+impl From<u32> for DescriptionFlags {
+    fn from(flags: u32) -> Self {
+        DescriptionFlags::from_bits_truncate(flags)
+    }
+}
+
+impl Into<u32> for DescriptionFlags {
+    fn into(self) -> u32 {
+        self.bits()
+    }
+}
+
+impl fmt::Display for DescriptionFlags {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+#[derive(Debug)]
+/// Format description as returned by VIDIOC_ENUM_FMT
+pub struct Description {
+    pub index: u32,
+    pub typ: u32,
+    pub flags: DescriptionFlags,
+    pub description: String,
+    pub fourcc: FourCC,
+}
+
+impl fmt::Display for Description {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "index       : {}", self.index)?;
+        writeln!(f, "type:       : {}", self.typ)?;
+        writeln!(f, "flags:      : {}", self.flags)?;
+        writeln!(f, "description : {}", self.description)?;
+        writeln!(f, "fourcc      : {}", self.fourcc)?;
+        Ok(())
+    }
+}
+
+impl From<v4l2_fmtdesc> for Description {
+    fn from(desc: v4l2_fmtdesc) -> Self {
+        Description {
+            index: desc.index,
+            typ: desc.type_,
+            flags: DescriptionFlags::from(desc.flags),
+            description: str::from_utf8(&desc.description)
+                .unwrap()
+                .trim_matches(char::from(0))
+                .to_string(),
+            fourcc: FourCC::from(desc.pixelformat),
+        }
+    }
+}
+
+bitflags! {
+    #[allow(clippy::unreadable_literal)]
+    pub struct Flags : u32 {
+        const PREMUL_ALPHA  = 0x00000001;
     }
 }
 
@@ -36,42 +97,6 @@ impl fmt::Display for Flags {
     }
 }
 
-#[derive(Debug)]
-/// Format description as returned by VIDIOC_ENUM_FMT
-pub struct Description {
-    pub index: u32,
-    pub typ: u32,
-    pub flags: Flags,
-    pub description: String,
-    pub fourcc: FourCC,
-}
-
-impl fmt::Display for Description {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "index       : {}", self.index)?;
-        writeln!(f, "type:       : {}", self.typ)?;
-        writeln!(f, "flags:      : {}", self.flags)?;
-        writeln!(f, "description : {}", self.description)?;
-        writeln!(f, "fourcc      : {}", self.fourcc)?;
-        Ok(())
-    }
-}
-
-impl From<v4l2_fmtdesc> for Description {
-    fn from(desc: v4l2_fmtdesc) -> Self {
-        Description {
-            index: desc.index,
-            typ: desc.type_,
-            flags: Flags::from(desc.flags),
-            description: str::from_utf8(&desc.description)
-                .unwrap()
-                .trim_matches(char::from(0))
-                .to_string(),
-            fourcc: FourCC::from(desc.pixelformat),
-        }
-    }
-}
-
 #[derive(Debug, Copy, Clone)]
 /// Streaming format (single-planar)
 pub struct Format {
@@ -88,6 +113,9 @@ pub struct Format {
     pub stride: u32,
     /// maximum number of bytes required to store an image
     pub size: u32,
+
+    /// flags set by the application or driver
+    pub flags: Flags,
 
     /// supplements the pixelformat (fourcc) information
     pub colorspace: Colorspace,
@@ -120,6 +148,7 @@ impl Format {
             field_order: FieldOrder::Any,
             stride: 0,
             size: 0,
+            flags: Flags::from(0),
             colorspace: Colorspace::Default,
             quantization: Quantization::Default,
             transfer: TransferFunction::Default,
@@ -151,6 +180,7 @@ impl From<v4l2_pix_format> for Format {
             field_order: FieldOrder::try_from(fmt.field).expect("Invalid field order"),
             stride: fmt.bytesperline,
             size: fmt.sizeimage,
+            flags: Flags::from(fmt.flags),
             colorspace: Colorspace::try_from(fmt.colorspace).expect("Invalid colorspace"),
             quantization: Quantization::try_from(fmt.quantization).expect("Invalid quantization"),
             transfer: TransferFunction::try_from(fmt.xfer_func).expect("Invalid transfer function"),
@@ -172,6 +202,7 @@ impl Into<v4l2_pix_format> for Format {
         fmt.bytesperline = self.stride;
         fmt.sizeimage = self.size;
         fmt.colorspace = self.colorspace as u32;
+        fmt.flags = self.flags.into();
         fmt.quantization = self.quantization as u32;
         fmt.xfer_func = self.transfer as u32;
         fmt
