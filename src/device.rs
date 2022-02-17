raymanfx/libv4l-rs

commit 99ebd121806e792fc6b66941237f57f1f0ba96aa

control: Complete transition to VIDIOC_{G,S}_EXT_CTRLS API (#41)
Move away from the old VIDIOC_{G,S}_CTRL API.
The new API allows for larger data types (e.g. 64 bit integers) and
multi-dimensional controls.

Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

src/device.rs
@@ -93,14 +93,14 @@ impl Device {
     pub fn query_controls(&self) -> io::Result<Vec<control::Description>> {
         let mut controls = Vec::new();
         unsafe {
-            let mut v4l2_ctrl: v4l2_queryctrl = mem::zeroed();
+            let mut v4l2_ctrl: v4l2_query_ext_ctrl = mem::zeroed();
 
             loop {
                 v4l2_ctrl.id |= V4L2_CTRL_FLAG_NEXT_CTRL;
                 v4l2_ctrl.id |= V4L2_CTRL_FLAG_NEXT_COMPOUND;
                 match v4l2::ioctl(
                     self.handle().fd(),
-                    v4l2::vidioc::VIDIOC_QUERYCTRL,
+                    v4l2::vidioc::VIDIOC_QUERY_EXT_CTRL,
                     &mut v4l2_ctrl as *mut _ as *mut std::os::raw::c_void,
                 ) {
                     Ok(_) => {
@@ -171,11 +171,11 @@ impl Device {
     /// * `id` - Control identifier
     pub fn control(&self, id: u32) -> io::Result<Control> {
         unsafe {
-            let mut queryctrl: v4l2_queryctrl = mem::zeroed();
+            let mut queryctrl: v4l2_query_ext_ctrl = mem::zeroed();
             queryctrl.id = id;
             v4l2::ioctl(
                 self.handle().fd(),
-                v4l2::vidioc::VIDIOC_QUERYCTRL,
+                v4l2::vidioc::VIDIOC_QUERY_EXT_CTRL,
                 &mut queryctrl as *mut _ as *mut std::os::raw::c_void,
             )?;
 
@@ -183,19 +183,24 @@ impl Device {
             let description = control::Description::from(queryctrl);
 
             // query the actual control value
-            let mut v4l2_ctrl: v4l2_control = mem::zeroed();
+            let mut v4l2_ctrls: v4l2_ext_controls = mem::zeroed();
+            let mut v4l2_ctrl: v4l2_ext_control = mem::zeroed();
             v4l2_ctrl.id = id;
+            v4l2_ctrls.count = 1;
+            v4l2_ctrls.controls = &mut v4l2_ctrl as *mut v4l2_ext_control;
             v4l2::ioctl(
                 self.handle().fd(),
-                v4l2::vidioc::VIDIOC_G_CTRL,
+                v4l2::vidioc::VIDIOC_G_EXT_CTRLS,
                 &mut v4l2_ctrl as *mut _ as *mut std::os::raw::c_void,
             )?;
 
             let value = match description.typ {
                 control::Type::Integer | control::Type::Integer64 => {
-                    control::Value::Integer(v4l2_ctrl.value as i64)
+                    control::Value::Integer(v4l2_ctrl.__bindgen_anon_1.value64)
                 }
-                control::Type::Boolean => control::Value::Boolean(v4l2_ctrl.value == 1),
+                control::Type::Boolean => {
+                    control::Value::Boolean(v4l2_ctrl.__bindgen_anon_1.value64 == 1)
+                }
                 _ => {
                     return Err(io::Error::new(
                         io::ErrorKind::Other,

src/control.rs
@@ -1,6 +1,6 @@
 use bitflags::bitflags;
 use std::convert::{TryFrom, TryInto};
-use std::{fmt, mem, str};
+use std::{ffi, fmt, mem, str};
 
 use crate::v4l_sys::*;
 
@@ -151,13 +151,13 @@ pub struct Description {
     /// Name of the control, intended for the user
     pub name: String,
     /// Minimum value, inclusive
-    pub minimum: i32,
+    pub minimum: i64,
     /// Maximum value, inclusive
-    pub maximum: i32,
+    pub maximum: i64,
     /// Step size, always positive
-    pub step: i32,
+    pub step: u64,
     /// Default value
-    pub default: i32,
+    pub default: i64,
     /// Control flags
     pub flags: Flags,
 
@@ -165,14 +165,14 @@ pub struct Description {
     pub items: Option<Vec<(u32, MenuItem)>>,
 }
 
-impl From<v4l2_queryctrl> for Description {
-    fn from(ctrl: v4l2_queryctrl) -> Self {
+impl From<v4l2_query_ext_ctrl> for Description {
+    fn from(ctrl: v4l2_query_ext_ctrl) -> Self {
         Description {
             id: ctrl.id,
             typ: Type::try_from(ctrl.type_).unwrap(),
-            name: str::from_utf8(&ctrl.name)
+            name: unsafe { ffi::CStr::from_ptr(ctrl.name.as_ptr()) }
+                .to_str()
                 .unwrap()
-                .trim_matches(char::from(0))
                 .to_string(),
             minimum: ctrl.minimum,
             maximum: ctrl.maximum,

src/v4l2/vidioc.rs
@@ -139,3 +139,4 @@ pub const VIDIOC_ENUM_FRAMEINTERVALS: _IOC_TYPE = _IOWR!(b'V', 75, v4l2_frmivale
 pub const VIDIOC_G_ENC_INDEX: _IOC_TYPE = _IOR!(b'V', 76, v4l2_enc_idx);
 pub const VIDIOC_ENCODER_CMD: _IOC_TYPE = _IOWR!(b'V', 77, v4l2_encoder_cmd);
 pub const VIDIOC_TRY_ENCODER_CMD: _IOC_TYPE = _IOWR!(b'V', 78, v4l2_encoder_cmd);
+pub const VIDIOC_QUERY_EXT_CTRL: _IOC_TYPE = _IOWR!(b'V', 103, v4l2_query_ext_ctrl);

commit 5642812c8984cd571c7355b5cd7b38386edf5613

device: Use ctrls instead of ctrl structure in VIDIOC_G_EXT_CTRLS
The v4l2_ctrls struct was unused (found after converting the assignments
to a struct initializer, expect a bigger PR for that later), and
v4l2_ctrl was erroneously passed as pointer argument to
VIDIOC_G_EXT_CTRLS - this should have been v4l2_ctrls.

Fixes: 99ebd12 ("control: Complete transition to VIDIOC_{G,S}_EXT_CTRLS API (#41)")

src/device.rs
@@ -191,7 +191,7 @@ impl Device {
             v4l2::ioctl(
                 self.handle().fd(),
                 v4l2::vidioc::VIDIOC_G_EXT_CTRLS,
-                &mut v4l2_ctrl as *mut _ as *mut std::os::raw::c_void,
+                &mut v4l2_ctrls as *mut _ as *mut std::os::raw::c_void,
             )?;
 
             let value = match description.typ {