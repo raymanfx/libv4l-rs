1ca7ff167e34c964516d14d9a02c412c545c80cb
Christopher N. Hesse <raymanfx@gmail.com>
2023-05-07 20:57:54 +0200

    sys: Remove deprecated unaligned_references lint
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- v4l-sys/src/lib.rs
+++ v4l-sys/src/lib.rs
@@ -3,7 +3,5 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
-// https://github.com/rust-lang/rust-bindgen/issues/1651
-#![allow(unaligned_references)]
 
 include!(concat!(env!("OUT_DIR"), "/libv4l_bindings.rs"));
--- v4l2-sys/src/lib.rs
+++ v4l2-sys/src/lib.rs
@@ -3,7 +3,5 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
-// https://github.com/rust-lang/rust-bindgen/issues/1651
-#![allow(unaligned_references)]
 
 include!(concat!(env!("OUT_DIR"), "/v4l2_bindings.rs"));
