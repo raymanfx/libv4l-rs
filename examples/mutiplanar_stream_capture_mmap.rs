b260262170ef715364f713a87d53bca3482829ea
XxChang <xuxchang@hotmail.com>
2024-04-01 10:23:42 +0800

    minor

--- examples/mutiplanar_stream_capture_mmap.rs
+++ examples/mutiplanar_stream_capture_mmap.rs
@@ -36,33 +36,33 @@ fn main() -> io::Result<()> {
 
     mplane_stream.next()?;
 
-    // let start = Instant::now();
-    // let mut megabytes_ps: f64 = 0.0;
-    // for i in 0..count {
-    //     let t0 = Instant::now();
-    //     let (buf, meta) = mplane_stream.next()?;
-    //     let duration_us = t0.elapsed().as_micros();
+    let start = Instant::now();
+    let mut megabytes_ps: f64 = 0.0;
+    for i in 0..count {
+        let t0 = Instant::now();
+        let (buf, meta) = mplane_stream.next()?;
+        let duration_us = t0.elapsed().as_micros();
 
-    //     let cur = buf.len() as f64 / 1_048_576.0 * 1_000_000.0 / duration_us as f64;
-    //     if i == 0 {
-    //         megabytes_ps = cur;
-    //     } else {
-    //         // ignore the first measurement
-    //         let prev = megabytes_ps * (i as f64 / (i + 1) as f64);
-    //         let now = cur * (1.0 / (i + 1) as f64);
-    //         megabytes_ps = prev + now;
-    //     }
+        let cur = buf.len() as f64 / 1_048_576.0 * 1_000_000.0 / duration_us as f64;
+        if i == 0 {
+            megabytes_ps = cur;
+        } else {
+            // ignore the first measurement
+            let prev = megabytes_ps * (i as f64 / (i + 1) as f64);
+            let now = cur * (1.0 / (i + 1) as f64);
+            megabytes_ps = prev + now;
+        }
 
-    //     println!("Buffer");
-    //     println!("  sequence  : {}", meta.sequence);
-    //     println!("  timestamp : {}", meta.timestamp);
-    //     println!("  flags     : {}", meta.flags);
-    //     println!("  length    : {}", buf.len());
-    // }    
+        println!("Buffer");
+        println!("  sequence  : {}", meta.sequence);
+        println!("  timestamp : {}", meta.timestamp);
+        println!("  flags     : {}", meta.flags);
+        println!("  length    : {}", buf.len());
+    }    
 
-    // println!();
-    // println!("FPS: {}", count as f64 / start.elapsed().as_secs_f64());
-    // println!("MB/s: {}", megabytes_ps);
+    println!();
+    println!("FPS: {}", count as f64 / start.elapsed().as_secs_f64());
+    println!("MB/s: {}", megabytes_ps);
 
     Ok(())
 }
