4edb60c2552cf84700abaca0484dc456a1726586
XxChang <xuxchang@hotmail.com>
2024-04-01 10:16:30 +0800

    minor

--- examples/mutiplanar_stream_capture_mmap.rs
+++ examples/mutiplanar_stream_capture_mmap.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::time::Instant;
 
 use v4l::buffer::Type;
 use v4l::capability::Flags;
@@ -29,10 +30,39 @@ fn main() -> io::Result<()> {
     format.field_order = FieldOrder::Interlaced;
     dev.set_format(&format)?;
 
+    let count = 4;
+
     let mut mplane_stream = MPlaneStream::new(&dev, Type::VideoCaptureMplane, 1)?;
 
     mplane_stream.next()?;
 
+    let start = Instant::now();
+    let mut megabytes_ps: f64 = 0.0;
+    for i in 0..count {
+        let t0 = Instant::now();
+        let (buf, meta) = mplane_stream.next()?;
+        let duration_us = t0.elapsed().as_micros();
+
+        let cur = buf.len() as f64 / 1_048_576.0 * 1_000_000.0 / duration_us as f64;
+        if i == 0 {
+            megabytes_ps = cur;
+        } else {
+            // ignore the first measurement
+            let prev = megabytes_ps * (i as f64 / (i + 1) as f64);
+            let now = cur * (1.0 / (i + 1) as f64);
+            megabytes_ps = prev + now;
+        }
+
+        println!("Buffer");
+        println!("  sequence  : {}", meta.sequence);
+        println!("  timestamp : {}", meta.timestamp);
+        println!("  flags     : {}", meta.flags);
+        println!("  length    : {}", buf.len());
+    }    
+
+    println!();
+    println!("FPS: {}", count as f64 / start.elapsed().as_secs_f64());
+    println!("MB/s: {}", megabytes_ps);
     
     Ok(())
 }
